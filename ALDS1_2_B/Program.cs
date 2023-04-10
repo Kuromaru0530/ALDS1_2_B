5f72ef9b270ff28d0f45e3b0da8b998662abe208
Kuromaru0530 <kuromary77@gmail.com>
2023-04-10 16:40:39 +0900

    プロジェクト ファイルを追加します。

---
+++ ALDS1_2_B/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var In = Console.ReadLine().Split(' ');
+        var a = Array.ConvertAll(In, x => int.Parse(x));
+        var Count = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            int minj = i;
+            for(int j = i; j < n; j++)
+            {
+                if (a[j] < a[minj])
+                {
+                     minj = j;
+                    
+                }
+            }
+            if(i != minj)
+            {
+                int x = 0;
+
+                x = a[i];
+                a[i] = a[minj];
+                a[minj] = x;
+                Count++;
+            }
+            
+
+            
+        }
+        Console.WriteLine(string.Join(" ", a));
+        Console.WriteLine(Count);
+    }  
+}
